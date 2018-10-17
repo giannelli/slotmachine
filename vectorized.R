79cf0b6b143931f069f920c69b1521ea2a42efc7
John Giannelli <john.giannelli@sky.uk>
2018-10-17 16:30:21 +0100

    Add more to vectorized for chapter 10

--- vectorized.R
+++ vectorized.R
@@ -150,3 +150,92 @@ get_many_symbols <- function(n) {
 }
 get_many_symbols(10)
 
+# symbols should be a matrix with a column for each slot machine window
+score_many <- function(symbols) {
+  # Step 1: Assign base prize based on cherries and diamonds ---------
+  ## Count the number of cherries and diamonds in each combination
+  cherries <- rowSums(symbols == "C")
+  diamonds <- rowSums(symbols == "DD")
+  ## Wild diamonds count as cherries
+  prize <- c(0, 2, 5)[cherries + diamonds + 1]
+  ## ...but not if there are zero real cherries
+  ### (cherries is coerced to FALSE where cherries == 0)
+  prize[!cherries] <- 0
+  # Step 2: Change prize for combinations that contain three of a kind
+  same <- symbols[, 1] == symbols[, 2] &
+    symbols[, 2] == symbols[, 3]
+  payoffs <- c("DD" = 100, "7" = 80, "BBB" = 40,
+               "BB" = 25, "B" = 10, "C" = 10, "0" = 0)
+  prize[same] <- payoffs[symbols[same, 1]]
+  # Step 3: Change prize for combinations that contain all bars ------
+  bars <- symbols == "B" | symbols == "BB" | symbols == "BBB"
+  all_bars <- bars[, 1] & bars[, 2] & bars[, 3] & !same
+  prize[all_bars] <- 5
+  # Step 4: Handle wilds ---------------------------------------------
+  ## combos with two diamonds
+  two_wilds <- diamonds == 2
+  ### Identify the nonwild symbol
+  one <- two_wilds & symbols[, 1] != symbols[, 2] &
+    symbols[, 2] == symbols[, 3]
+  two <- two_wilds & symbols[, 1] != symbols[, 2] &
+    symbols[, 1] == symbols[, 3]
+  three <- two_wilds & symbols[, 1] == symbols[, 2] &
+    symbols[, 2] != symbols[, 3]
+  ### Treat as three of a kind
+  prize[one] <- payoffs[symbols[one, 1]]
+  prize[two] <- payoffs[symbols[two, 2]]
+  prize[three] <- payoffs[symbols[three, 3]]
+  ## combos with one wild
+  one_wild <- diamonds == 1
+  ### Treat as all bars (if appropriate)
+  wild_bars <- one_wild & (rowSums(bars) == 2)
+  prize[wild_bars] <- 5
+  ### Treat as three of a kind (if appropriate)
+  one <- one_wild & symbols[, 1] == symbols[, 2]
+  two <- one_wild & symbols[, 2] == symbols[, 3]
+  three <- one_wild & symbols[, 3] == symbols[, 1]
+  prize[one] <- payoffs[symbols[one, 1]]
+  prize[two] <- payoffs[symbols[two, 2]]
+  prize[three] <- payoffs[symbols[three, 3]]
+  # Step 5: Double prize for every diamond in combo ------------------
+  unname(prize * 2^diamonds)
+}
+  
+play_many <- function(n) {
+  symb_mat <- get_many_symbols(n = n)
+  data.frame(w1 = symb_mat[,1], w2 = symb_mat[,2],
+             w3 = symb_mat[,3], prize = score_many(symb_mat))
+}
+
+system.time(play_many(1000000))
+
+## Vectorised scoring - Ten Million!
+system.time({
+  plays <- play_many(10000000)
+  print(mean(plays$prize))
+})
+# [1] 0.9371745
+# user  system elapsed 
+# 8.706   1.917  10.622 
+
+## Non vectorised scoring - 'Only' One Million 
+system.time({
+  winnings <- vector(length = 1000000)
+  for (i in 1:1000000) {
+    winnings[i] <- play()
+  }
+  print(paste("Average win: ", mean(winnings)))
+})
+# [1] "Average win:  0.93637"
+# user  system elapsed 
+# 39.424   0.002  39.420 
+
+#######################
+symbols <- matrix(
+  c("DD", "DD", "DD",
+    "C", "DD", "0",
+    "B", "B", "B",
+    "B", "BB", "BBB",
+    "C", "C", "0",
+    "7", "DD", "DD"), nrow = 6, byrow = TRUE)
+symbols
