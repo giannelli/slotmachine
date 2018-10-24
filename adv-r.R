d568f14cec637effe96533cd7ca8b92b77b6acb5
John Giannelli <john.giannelli@sky.uk>
2018-10-24 14:22:50 +0100

    More examples from Advanced R

--- adv-r.R
+++ adv-r.R
@@ -317,3 +317,247 @@ obj_size(y)
 
 # Should be zero!
 obj_size(y) - obj_size(x) - obj_size(list(NULL,NULL,NULL,NULL,NULL,NULL,NULL))
+
+
+# 2.4.1 Exercises
+# 1.
+y <- rep(list(runif(1e4)), 100)
+y
+object.size(y)
+obj_size(y)
+?obj_size
+ref(y)
+
+y[[3]][1]
+y[[99]][1]
+
+# 2.
+x <- list(mean, sd, var)
+obj_size(x)
+
+
+# 3.
+?runif
+
+# 2.5 Modify-in-place
+
+v <- c(1,2,3)
+tracemem(v)
+v[[3]] <- 4
+
+untracemem(v)
+
+
+x <- data.frame(matrix(runif(5 * 1e4), ncol = 5))
+medians <- vapply(x, median, numeric(1))
+x
+medians
+
+cat(tracemem(x), "\n")
+for (i in seq_along(medians)) {
+  x[[i]] <- x[[i]] - medians[[i]]
+}
+untracemem(x)
+
+
+y <- as.list(x)
+cat(tracemem(y), "\n")
+
+for (i in 1:5) {
+  y[[i]] <- y[[i]] - medians[[i]]
+}
+untracemem(y)
+
+# 2.5.2 Environments
+
+e1 <- rlang::env(a=1, b= 2, c=3)
+e2 <- e1
+
+e1$c <- 4
+
+e2$c
+
+# http://bench.r-lib.org/
+# devtools::install_github("r-lib/bench")
+
+
+
+sub_median1 <- function(numcolumns = 5) {
+  x <- data.frame(matrix(runif(5 * 1e4), ncol = numcolumns))
+  medians <- vapply(x, median, numeric(1))
+  for (i in seq_along(medians)) {
+    x[[i]] <- x[[i]] - medians[[i]]
+  }
+}
+
+sub_median2 <- function(numcolumns = 5) {
+  x <- data.frame(matrix(runif(5 * 1e4), ncol = numcolumns))
+  medians <- vapply(x, median, numeric(1))
+  for (i in seq_along(medians)) {
+    x[[i]] <- x[[i]] - medians[[i]]
+  }
+}
+
+library(bench)
+bnch1 <- bench::mark(sub_median1())
+bnch1
+bnch2 <- bench::mark(sub_median2())
+bnch2
+
+#?press
+# Helper function to create a simple data.frame of the specified dimensions
+create_df <- function(rows, cols) {
+  as.data.frame(setNames(
+    replicate(cols, runif(rows, 1, 1000), simplify = FALSE),
+    rep_len(c("x", letters), cols)))
+}
+
+# Run 4 data sizes across 3 samples with 2 replicates (24 total benchmarks)
+press(
+  rows = c(1000, 10000),
+  cols = c(10, 100),
+  rep = 1:2,
+  {
+    dat <- create_df(rows, cols)
+    bench::mark(
+      min_time = .05,
+      bracket = dat[dat$x > 500, ],
+      which = dat[which(dat$x > 500), ],
+      subset = subset(dat, x > 500)
+    )
+  }
+)
+
+press(
+  cols = c(5, 10, 15, 20, 100, 1000),
+  rep = 1:2,
+  {
+    bench::mark(
+      sub_median1(cols)
+    )
+  }
+)
+
+press(
+  cols = c(5, 10, 15, 20, 100, 1000),
+  rep = 1:2,
+  {
+    bench::mark(
+      sub_median2(cols)
+    )
+  }
+)
+
+mem_used()
+gc()
+
+####  3 Vectores  ################
+
+# 3.2 Atomic Vectors
+dbl_var <- c(1, 2.5, 4.5)
+int_var <- c(1L, 6L, 10L)
+lgl_var <- c(TRUE, FALSE)
+chr_var <- c("these are", "some strings")
+
+typeof(dbl_var)
+typeof(int_var)
+typeof(lgl_var)
+typeof(chr_var)
+
+str(c("a", 1))
+# ?str
+# str(x)
+
+# Coercion
+x <- c(FALSE, FALSE, TRUE)
+as.numeric(x)
+sum(x)
+mean(x)
+
+as.integer(c("1", "1.5", "a"))
+
+# 3.2.4 Exercises
+
+# 1.
+
+# ?raw
+xx <- raw(2)
+xx
+typeof(xx)
+xx <- raw(123)
+xx
+typeof(xx)
+
+# ?complex
+c1 <- 0i ^ (-3:3)
+c1
+
+# 2. 
+typeof(c(1, FALSE))
+typeof(c("a", 1))
+typeof(c(TRUE, 1L))
+
+# 3. 
+
+1 == "1"
+-1 < FALSE
+"one" < 2
+"1" < 2
+typeof(c("1", 2))
+
+# 4.
+
+typeof(c(TRUE, FALSE))
+typeof(c(FALSE, NA_character_))
+typeof(c(FALSE, NA))
+
+# 5.
+x <- c(1,2,3)
+y <- c(1,2,"3")
+z <- list(1, 2, 3)
+# zz <- list(1, "2", 3) 
+is.atomic(x)
+is.atomic(y)
+is.atomic(z)
+
+is.numeric(x)
+is.numeric(y)
+is.numeric(z)
+
+is.vector(x)
+is.vector(y)
+is.vector(z)
+
+
+# 3.3.1  Getting and setting
+a <- 1:3
+attr(a, "x") <- "abcdef"
+attr(a, "x")
+
+attr(a, "y") <- 4:6
+str(attributes(a))
+
+a <- structure(
+  1:3, 
+  x = "abcdef",
+  y = 4:6
+)
+str(attributes(a))
+
+# 3.3.2 Names
+
+# When creating it: 
+x <- c(a = 1, b = 2, c = 3)
+x
+
+# By assigning names() to an existing vector:
+x <- 1:3
+names(x) <- c("a", "b", "c")
+x
+
+# Inline, with setNames():
+x <- setNames(1:3, c("a", "b", "c"))
+x
+
+# 3.3.3 Dimensions
+
